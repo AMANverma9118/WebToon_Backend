122f1507fb5093b7cc0a2425a053a64c84795cdc
AMANverma9118 <AMANverma9118359330@gmail.com>
2024-10-14 00:39:46 +0530

    API for adding new data is completed

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# It is an API which store,fetch all data,fetch data by id,update data by id and delet data by id
+
+# It also store image according to enter id with it's own id on cloud and also save in database as link to access at any place
+
+
+
+env file:-
+
+URI = "mongodb+srv://Aman:Aman9118234@cluster0.volli.mongodb.net/"
+PORT = 5000
+JWT_SECRETE = "WEBTOONBACKEND"
+CLOUDINARY_CLOUD_NAME = dln46znfe
+CLOUDINARY_API_KEY = 865242383785152
+CLOUDINARY_SECKRET_KEY = yLgLpUFhb_eM7btF2BHjKm2-uyk
--- Routes/Routes.js
+++ Routes/Routes.js
@@ -19,5 +19,61 @@ router.get('/fetchallwebtoons', async (req, res) => {
 
 
 
+// Route 2: Add a new webtoon (POST /api/webtoons/createwebtoon) and It also store image in cloudinary
+router.post('/createwebtoon', upload.single('image'), [
+    body('title', 'Title is required and should be at least 3 characters').isLength({ min: 3 }),
+    body('description', 'Description must be at least 5 characters').isLength({ min: 5 }),
+    body('characters', 'Characters array is required').isArray()
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const { title, description, characters } = req.body;
+
+
+        if (!req.file) {
+            return res.status(400).json({ error: "Image file is required." });
+        }
+
+
+        const cloudinaryResult = await uploadToCloudinary(req.file.buffer, `${title}-${Date.now()}`);
+        const imageUrl = cloudinaryResult.secure_url;
+
+
+        const newWebtoon = new Webtoon({
+            title,
+            description,
+            characters,
+            imageUrl
+        });
+
+
+        const savedWebtoon = await newWebtoon.save();
+
+
+        const data = {
+            webtoon: {
+                id: savedWebtoon.id
+            }
+        };
+        const authToken = jwt.sign(data, process.env.JWT_SECRETE, { expiresIn: '1h' });
+
+        res.json({
+            webtoon: savedWebtoon,
+            authToken: authToken
+        });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+
 
 module.exports = router;
